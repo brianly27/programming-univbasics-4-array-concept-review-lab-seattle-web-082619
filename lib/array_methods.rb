bb0446fff8ceeb94d893f2ba8e95be45aac85bfd
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-26 16:30:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   while array[counter] do
     value = array[counter]
     if find = value
-      find = counter  
+      find = counter
     end
     counter += 1
   end
