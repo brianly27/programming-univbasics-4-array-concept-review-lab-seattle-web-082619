71c7139becf12cd56a0bd29dc78899eb839a7949
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-26 16:30:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,14 @@
 def find_element_index(array, value_to_find)
-
-
+  counter = 0
+  find = value_to_find
+  while array[counter] do
+    value = array[counter]
+    if find = value
+      find = counter  
+    end
+    counter += 1
+  end
+  find
 end
 
 def find_max_value(array)
